fc583792df3bde275d012dd5879d5da2da394e06
ihyaet <17523126@students.uii.ac.id>
2019-07-01 15:06:16 +0700

    landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -22,16 +22,126 @@
 
 <body>
 
-  <div class="container-full-bg">
+  <div class="container-full-bg shadow">
     <div class="jumbotron">
-      <div class="row justify-content-center">
+      <div class="row justify-content-center m-0 p-0">
          <img src="img/bnpb.png" style="height: 350x; width: 350px;">
       </div>
-      <div class="row justify-content-center m-3">
-      <h3 style="color:white; ">SISTEM DISTRIBUSI LOGISTIK</h3>
+      <div class="row justify-content-center m-4">
+        <h3 style="color: white; font-weight: bolder; ">SISTEM DISTRIBUSI LOGISTIK</h3>
+        <p class="text-center m-2" style="color:#ededed; font-size: 18px; letter-spacing: 2px;">Sistem Distribusi Logistik (SISLOG) merupakan suatu sistem berbasis website yang digunakan untuk manajemen gudang logistik di PUSDALOPS BPBD
+        </p>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-lg-6 text-center">
+           <a href="" style="border-radius: 20px;" class="btn btn-info btn-user text-center">Kelola Gudang</a>
+        </div>
       </div>
     </div>
-    
+
+    <section class="bg-white">
+      <div class="container">
+        <div class="row  justify-content-center">
+            <p class="m-4"><strong><font>TIM STARTBUG</font></strong></p>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col">
+              <div class="card shadow mb-4">
+                <div class="card-body">
+                  <div class="row justify-content-center">
+                    <img src="https://instagram.fcgk16-1.fna.fbcdn.net/vp/e297811a7dbc88118b0833dd91cb44af/5DC1B0FE/t51.2885-15/e35/43653367_543410486124558_3114722360144129502_n.jpg?_nc_ht=instagram.fcgk16-1.fna.fbcdn.net" class="rounded-circle mt-0" style="width: 180px; height: 180px;">
+                  </div>
+                  <div class="row justify-content-center">
+                    <h5 class="mt-3" style="font-weight: bold;">Ihya Fathurrahman</h5>
+                    <p class="justify-content-center" style="font-size: 15px;">17523126@students.uii.ac.id</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+                  <div class="card shadow mb-4">
+                <div class="card-body">
+                  <div class="row justify-content-center">
+                    <img src="https://scontent-sin6-2.cdninstagram.com/vp/85f09dadd3b46e851ffaa0808fe6fbab/5DB48F6D/t51.2885-19/s150x150/43051264_272845183589257_3694189181895966720_n.jpg?_nc_ht=scontent-sin6-2.cdninstagram.com" class="rounded-circle mt-0" style="width: 180px; height: 180px;">
+                  </div>
+                  <div class="row justify-content-center">
+                    <h5 class="mt-3" style="font-weight: bold;">Muhammad Rijal Kamal</h5>
+                    <p class="justify-content-center" style="font-size: 15px;">17523098@students.uii.ac.id</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+                <div class="card shadow mb-4">
+                <div class="card-body">
+                  <div class="row justify-content-center">
+                    <img src="https://scontent-sin6-2.cdninstagram.com/vp/07376f2e58ce2cec72e92e14c817bc18/5DAD721E/t51.2885-19/s150x150/64281386_438520696725975_6252362189764558848_n.jpg?_nc_ht=scontent-sin6-2.cdninstagram.com" class="rounded-circle mt-0" style="width: 180px; height: 180px;">
+                  </div>
+                  <div class="row justify-content-center">
+                    <h5 class="mt-3" style="font-weight: bold;">Diah Hanifah Putri</h5>
+                    <p class="justify-content-center" style="font-size: 15px;">17523144@students.uii.ac.id</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+                <div class="card shadow mb-4">
+                <div class="card-body">
+                  <div class="row justify-content-center">
+                    <img src="https://scontent-sin6-2.cdninstagram.com/vp/26e8de27ca9ee96f60dcf0a12b159078/5DB922E0/t51.2885-15/sh0.08/e35/s750x750/60224039_414013802660682_4813467983225204121_n.jpg?_nc_ht=scontent-sin6-2.cdninstagram.com" class="rounded-circle mt-0" style="width: 180px; height: 180px;">
+                  </div>
+                  <div class="row justify-content-center">
+                    <h5 class="mt-3" style="font-weight: bold;">Lugie Nur Ahmad</h5>
+                    <p class="justify-content-center" style="font-size: 15px;">17523138@students.uii.ac.id</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </section>
+    <section class="bg-gradient-info">
+      <div class="container" style="color: white;">
+        <footer class="page-footer font-small blue pt-4">
+            <div class="container">
+              <div class="container-fluid text-center text-md-left">
+                <div class="row">
+                  <div class="col-md-6 mt-md-0 mt-3">
+                    <h5 class="text-uppercase"><b>SISLOG</b></h5>
+                    <p>"Sistem Distribusi Logistik"</p>
+                    <!-- <p>Pastisipasi anda sangat kami harapakan dan kami tunggu.</p> -->
+                  </div>
+                  <hr class="clearfix w-100 d-md-none pb-3">
+                  <div class="col-md-3 mb-md-0 mb-3">
+
+                    </div>
+
+                    <div class="col-md-3 mb-md-0 mb-3 text-md-right">
+                      <h5 class="text-uppercase"><b>Kontak dan Alamat</b></h5>
+                      <ul class="list-unstyled">
+                        <li>
+                         <p>Jalan Kaliurang KM 14.5, Sleman, Yogyakarta</p>
+                        </li>
+                        <li>
+                          <p>(0274)138009</p>
+                        </li>
+                        <li>
+                          <p>support.admin@startbug.com</p>
+                        </li>
+                      </ul>
+                    </div>
+                </div>
+              </div>
+            </div>
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3">© 2019 STARTBUG</div>
+        <!-- Copyright -->
+        </footer>
+        
+      </div>
+    </section>
+
+
   </div>
 
   <!-- Bootstrap core JavaScript-->
