dbe0901952ee0c7526d2a40cc389b633f9d03710
ihyaet <52045442+ihyaet@users.noreply.github.com>
2019-07-01 12:02:34 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    HENSHIN
+                    HENSHIN 10
                 </div>
 
                 <div class="links">
